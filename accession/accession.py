ce4514ca26b13b3c86f1e47d914f96616b470ca0
Paul Sud <41386393+p-sud@users.noreply.github.com>
2020-04-02 11:07:01 -0700

    PIP-969 add mint chip multple blacklist template (#50)

--- accession/accession.py
+++ accession/accession.py
@@ -1204,6 +1204,7 @@ def accession_factory(
         "chip_map_only": AccessionChip,
         "tf_chip_peak_call_only": AccessionChip,
         "histone_chip_peak_call_only": AccessionChip,
+        "mint_chip_peak_call_only": AccessionChip,
     }
     selected_accession: Optional[Type[Accession]] = None
     try:
