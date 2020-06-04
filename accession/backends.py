0cc7956209843f0940a1f52488b29b81297a70f8
Paul Sud <paulsud@stanford.edu>
2020-06-04 10:45:03 -0700

    add fix and tests

--- accession/backends.py
+++ accession/backends.py
@@ -109,19 +109,20 @@ class GcsBlob(storage.blob.Blob):
         range. These are 0-indexed and inclusive of endpoints. If the position is
         greater than or equal to the size of the object then we treat that as EOF and
         return an empty byte string `b''`. As per Python convention, when read() is
-        called with no read size then the whole file is returned.
+        called with no read size then the remainder of the file is returned.
 
         See https://googleapis.dev/python/storage/latest/blobs.html#google.cloud.storage.blob.Blob.download_as_string
         and https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.upload_fileobj
         """
-        if num_bytes is None:
-            return self.download_as_string()
+        if self.pos >= self.size:
+            read_bytes = b""
         else:
-            if self.pos >= self.size:
-                read_bytes = b""
+            if num_bytes is None:
+                read_bytes = self.download_as_string(start=self.pos)
+                self.pos += len(read_bytes)
             else:
                 read_bytes = self.download_as_string(
                     start=self.pos, end=self.pos + num_bytes - 1
                 )
                 self.pos += num_bytes
-            return read_bytes
+        return read_bytes
