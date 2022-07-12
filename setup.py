78305dfd2e66656bb04f66890e8b6555b200ed8e
Otto Jolanki <ojolanki@stanford.edu>
2022-07-12 15:12:42 -0700

    typo in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ CLASSIFIERS = [
 INSTALL_REQUIRES = [
     "SQLAlchemy==1.3.17",
     "attrs",
-    "protobuf==3.20.1"
+    "protobuf==3.20.1",
     "boto3==1.17.108",
     "caper==1.5.1",
     "encode_utils==2.10.0",
